dee78fae265aec43861cfd8b7523e6724407289f
Will Reichl <William.Reichl@dcsg.com>
2020-03-09 06:56:23 -0400

    Hello world example

--- Ch2/helloworld_start.py
+++ Ch2/helloworld_start.py
@@ -2,3 +2,8 @@
 # Example file for HelloWorld
 #
 
+def main(): # defines a function, does nothing until invoked
+    print("hello world")
+
+if __name__ == "__main__": # essentially checks if main was called from the command line, as opposed to included in another program
+    main()
