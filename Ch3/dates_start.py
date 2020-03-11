78932b27ce5c5e0f553615b4fa8b2d4a8fd36ae5
Will Reichl <William.Reichl@dcsg.com>
2020-03-10 21:59:47 -0400

    Python current day/time

--- Ch3/dates_start.py
+++ Ch3/dates_start.py
@@ -1,27 +1,33 @@
 #
 # Example file for working with date information
 #
+from datetime import date, time, datetime  # from = library, import = classes
 
 
 def main():
-  ## DATE OBJECTS
-  # Get today's date from the simple today() method from the date class
+    # DATE OBJECTS
+    # Get today's date from the simple today() method from the date class
+    today = date.today()
+    print("Today's date is ", today)
+
+    # print out the date's individual components
+    print("Date components: ", today.day, today.month, today.year)
+
+    # retrieve today's weekday (0=Monday, 6=Sunday)
+    print("Today's weekday number is: ", today.weekday())
+    days = ["monday", "tuesday", "wednesday",
+            "thursday", "friday", "saturday", "sunday"]
+    print("Which is a " + days[today.weekday()])
+
+    # DATETIME OBJECTS
+    # Get today's date from the datetime class
+    today = datetime.now()
+    print("The current date and time is ", today)
+
+    # Get the current time
+    t = datetime.time(datetime.now())
+    print(t)
 
 
-  # print out the date's individual components
-
-  
-  # retrieve today's weekday (0=Monday, 6=Sunday)
-
-  
-  ## DATETIME OBJECTS
-  # Get today's date from the datetime class
-
-
-  # Get the current time
-
-
-  
 if __name__ == "__main__":
-  main();
-  
+    main()
