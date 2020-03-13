a3d7fffb3d6cdf38b89c71c437f93a9ada3437a7
Will Reichl <William.Reichl@dcsg.com>
2020-03-12 20:30:17 -0400

    File and os paths

--- Ch4/files_start.py
+++ Ch4/files_start.py
@@ -2,21 +2,31 @@
 # Read and write files using the built-in Python file methods
 #
 
-def main():  
-  # Open a file for writing and create it if it doesn't exist
+
+def main():
+    # Open a file for writing and create it if it doesn't exist
+    filepath = "output/textfile.txt"
+    # f = open(filepath, "w+")
+
+    # Open the file for appending text to the end
+    f = open(filepath, "a")
+
+    # write some lines of data to the file
+    for i in range(10):
+        f.write("This is line " + str(i) + "\r\n")
+
+    # close the file when done
+    f.close()
+
+    # Open the file back up and read the contents
+    f = open(filepath, "r")
+    if f.mode == 'r':
+        # contents = f.read()
+        # print(contents)
+        fl = f.readlines()
+        for x in fl:
+            print(x)
 
 
-  # Open the file for appending text to the end
-
-
-  # write some lines of data to the file
-
-  
-  # close the file when done
-
-  
-  # Open the file back up and read the contents
-
-    
 if __name__ == "__main__":
-  main()
+    main()
--- Ch4/ospathutils_start.py
+++ Ch4/ospathutils_start.py
@@ -9,21 +9,28 @@ import time
 
 
 def main():
-  # Print the name of the OS
+    # Print the name of the OS
+    print(os.name)
 
+    # Check for item existence and type
+    print("Item exists: " + str(path.exists("output/textfile.txt")))
+    print("Item is a file: " + str(path.isfile("output/textfile.txt")))
+    print("Item is a directory: " + str(path.isdir("output/textfile.txt")))
+    print("Item is a directory: " + str(path.isdir("output")))
 
-  # Check for item existence and type
+    # Work with file paths
+    print("Item path: " + str(path.realpath("output/textfile.txt")))
+    print ("Item's path and name: " + str(path.split(path.realpath("output/textfile.txt"))))
 
-  
-  # Work with file paths
+    # Get the modification time
+    t = time.ctime(path.getmtime("output/textfile.txt"))
+    print(t)
+    print(datetime.datetime.fromtimestamp(path.getmtime("output/textfile.txt")))
 
-  
-  # Get the modification time
+    # Calculate how long ago the item was modified
+    td = datetime.datetime.now() - (datetime.datetime.fromtimestamp(path.getmtime("output/textfile.txt")))
+    print("It has been " + str(td) + " since the file was modified")
+    print("Or, " + str(td.total_seconds()) + " seconds")
 
-  
-  # Calculate how long ago the item was modified
-
-
-  
 if __name__ == "__main__":
-  main()
+    main()
